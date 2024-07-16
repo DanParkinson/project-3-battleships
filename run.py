abdf90d546fe51dc80b121d6f5c8d8c2c96d40b4
Daniel Parkinson <danny.parkinson1996@gmail.com>
2024-07-16 15:49:57 +0000

    Initial commit. Plan outlined

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# constants
+
+class Board:
+    '''
+    8x8 grid will be created
+    axis will be added to the gird
+    battleships will be created and stored in ship_locations
+    '''
+    pass
+
+class Game:
+    '''
+    create a hidden board used for only displaying
+    print boards for user
+    get user guess
+    get computer guess
+    validate guess'
+    update player and computer boards
+    update hidden board
+    check for game over
+    '''
+    pass
+
+def main():
+    '''
+    main function for playing game
+    '''
+    pass
+
+main()
